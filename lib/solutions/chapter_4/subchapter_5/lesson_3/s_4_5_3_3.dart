1f69b390602e29759d733a056fa18965a5039f57
Selihei <selina.heinrichlp@gmail.com>
2025-07-11 11:14:55 +0200

    solved 4.5.3.3

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_3.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_3.dart
@@ -4,6 +4,16 @@ class S4533 extends StatelessWidget {
   const S4533({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return Container(
+      height: 150,
+      width: 150,
+      color: Colors.blue,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+        Text("App"),
+        Text("Akademie")],
+      ),
+    );
   }
 }
