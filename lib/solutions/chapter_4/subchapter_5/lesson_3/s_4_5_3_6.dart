4fdbcb14e9414557441695b87649247d7700953e
Selihei <selina.heinrichlp@gmail.com>
2025-07-14 14:59:46 +0200

    solved 4.5.3.6

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_6.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_6.dart
@@ -4,6 +4,34 @@ class S4536 extends StatelessWidget {
   const S4536({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+List<Product> products = [
+Product("Apfel", 1.00),
+Product("Brot", 2.39),
+Product("Milch", 3.20),
+Product("Chips", 5.00),
+Product("Eis", 4.99)
+];
+
+    return ListView.builder(
+      itemCount: products.length,
+      shrinkWrap: true,
+      itemBuilder: (context, index){
+        final product = products[index];
+        return ListTile(
+          leading: const Icon(Icons.shopping_cart),
+          title: Text(product.name),
+          subtitle: Text("${product.price.toStringAsFixed(2)} €")
+        );
+      },
+      );
+      
+      
   }
+}
+
+class Product {
+  String name;
+  double price;
+
+  Product(this.name, this.price);
 }
